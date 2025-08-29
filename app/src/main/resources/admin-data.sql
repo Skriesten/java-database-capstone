84788afcf80eee1d3bd97be2cd2b82c3e66da174
SKriesten <32945963+Skriesten@users.noreply.github.com>
2025-08-29 14:09:16 -0400

    update the data samples
    updated the data sample files

--- app/src/main/resources/admin-data.sql
+++ app/src/main/resources/admin-data.sql
@@ -1,2 +1,3 @@
-INSERT INTO admin (username, password)
-VALUES ('admin', 'admin@1234');
+USE cms
+INSERT INTO admin (username, password, name, role, email, date_hired)
+VALUES ('admin', 'admin@1234',  'Jordan Taylor', 'admin',  'taylor@123.com',  '2021-02-15');
